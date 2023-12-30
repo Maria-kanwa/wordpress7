8036eeeaffddecd783bb8d3297edcbe123676486
unknown <you@example.com>
2023-12-30 16:58:16 +0500

    third commit

---
+++ index3.php
@@ -0,0 +1,22 @@
+<?php
+$num=9;
+if($num==1)
+{
+  echo "number is one";
+}
+elseif($num==2){
+  echo "number is two";
+}
+elseif($num==3){
+  echo "number is three";
+}
+elseif($num==4){
+  echo "number is four";
+}
+elseif($num==5){
+  echo "number is five";
+}
+else{
+  echo "number is not found";
+}
+?>
